8c8ab6f9305f415175170649ee16c247a554bd14
unknown <pemeraldy@gmail.com>
2020-02-10 21:35:17 +0100

    App layout setup

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const hbs = require("express-handlebars");
+const app = express();
+const { mongoDbUrl } = require("./config/configuration");
+/* Connect to DB */
+mongoose
+  .connect(mongoDbUrl, { useNewUrlParser: true })
+  .then(response => {
+    console.log("connected to db successfully");
+  })
+  .catch(err => {
+    console.log("DB conn failed", err);
+  });
+
+/* Configure middleware */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+/* setup view engine */
+app.engine("handlebars", hbs({ defaultLayout: "default" }));
+app.set("view engine", "handlebars");
+
+/* Routes */
+const defaultRouter = require("./routes/defaultRouter");
+app.use("/", defaultRouter);
+
+// Server started
+app.listen(4000, () => {
+  console.log("Server running on port 4k");
+});
---
+++ config/configuration.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoDbUrl: "mongodb://localhost:27017/cms",
+  PORT: process.env.PORT || 4000
+};
